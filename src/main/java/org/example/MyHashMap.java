17fabd24871efc659560fa441ad49cbe27c68fab
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-20 10:27:39 +0900

    1차 구현 완료

--- src/main/java/org/example/MyHashMap.java
+++ src/main/java/org/example/MyHashMap.java
@@ -1,15 +1,97 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class MyHashMap<K, V> {
 
-    private Object[] array = new Object[10];
+    private int initLength = 10;
+    private Object[] keys = new Object[initLength];
+    private Object[] values = new Object[initLength];
     private int size;
 
-    public V put(K key1, V i) {
+    public V put(K key, V value) {
+        if (size > 0) {
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i] != null && keys[i].equals(key)) {
+                    V removed = (V) values[i];
+                    values[i] = value;
+                    return removed;
+                }
+            }
+        }
+        if(size == initLength) {
+            initLength += 10;
+            keys = Arrays.copyOf(keys, initLength);
+            values = Arrays.copyOf(values, initLength);
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
+
         return null;
     }
 
     public int size() {
         return size;
+    }
+
+    public V get(K key) {
+        for (int i = 0; i < keys.length; i++) {
+            if (keys[i].equals(key)) {
+                return (V) values[i];
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        for(int i=0; i<keys.length; i++) {
+            if(keys[i] != null && keys[i].equals(key)) {
+                V removed = (V) values[i];
+                keys[i] = keys[i+1];
+                keys[i+1] = null;
+                values[i] = values[i+1];
+                values[i+1] = null;
+                size--;
+                return removed;
+            }
+        }
+        return null;
+    }
+
+    public boolean containsKey(K key) {
+        return Arrays.stream(keys)
+                .anyMatch(e -> e != null && e.equals(key));
+    }
+
+    public boolean containsValue(V value) {
+        return Arrays.stream(values)
+                .anyMatch(e -> e != null && e.equals(value));
+    }
+
+    public void clear() {
+        size = 0;
+        initLength = 10;
+        keys = new Object[initLength];
+        values = new Object[initLength];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for(int i=0; i<keys.length; i++) {
+            if(keys[i] != null && values[i] != null) {
+                sb.append(keys[i]).append("=").append(values[i]);
+                if (i != keys.length - 1 && keys[i+1] != null) sb.append(", ");
+            }
+        }
+        sb.append("}");
+
+        return sb.toString();
     }
 }
--- src/test/java/org/example/MyHashMapTest.java
+++ src/test/java/org/example/MyHashMapTest.java
@@ -17,63 +17,79 @@ class MyHashMapTest {
     void testPut() {
         assertNull(map.put("Key1", 1));
         assertEquals(1, map.size());
+        System.out.println(map);
     }
 
-//    @Test
-//    void testPutReplace() {
-//        map.put("Key1", 1);
-//        assertEquals(Integer.valueOf(1), map.put("Key1", 2));
-//        assertEquals(1, map.size());
-//    }
-//
-//    @Test
-//    void testGet() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        assertEquals(Integer.valueOf(1), map.get("Key1"));
-//        assertEquals(Integer.valueOf(2), map.get("Key2"));
-//    }
-//
-//    @Test
-//    void testRemove() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        assertEquals(Integer.valueOf(1), map.remove("Key1"));
-//        assertNull(map.remove("Key3"));
-//        assertEquals(1, map.size());
-//    }
-//
-//    @Test
-//    void testSize() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        map.put("Key3", 3);
-//        assertEquals(3, map.size());
-//    }
-//
-//    @Test
-//    void testContainsKey() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        assertTrue(map.containsKey("Key1"));
-//        assertFalse(map.containsKey("Key3"));
-//    }
-//
-//    @Test
-//    void testContainsValue() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        assertTrue(map.containsValue(1));
-//        assertFalse(map.containsValue(3));
-//    }
-//
-//    @Test
-//    void testClear() {
-//        map.put("Key1", 1);
-//        map.put("Key2", 2);
-//        map.put("Key3", 3);
-//        map.clear();
-//        assertEquals(0, map.size());
-//        assertTrue(map.isEmpty());
-//    }
+    @Test
+    void testPut11() {
+        for(int i=0; i<11; i++) {
+            map.put("key%d".formatted(i), i);
+        }
+        System.out.println(map);
+    }
+
+    @Test
+    void testPutReplace() {
+        map.put("Key1", 1);
+        assertEquals(Integer.valueOf(1), map.put("Key1", 2));
+        assertEquals(1, map.size());
+        System.out.println(map);
+    }
+
+    @Test
+    void testGet() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertEquals(Integer.valueOf(1), map.get("Key1"));
+        assertEquals(Integer.valueOf(2), map.get("Key2"));
+        System.out.println(map);
+    }
+
+    @Test
+    void testRemove() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertEquals(Integer.valueOf(1), map.remove("Key1"));
+        assertNull(map.remove("Key3"));
+        assertEquals(1, map.size());
+        System.out.println(map);
+    }
+
+    @Test
+    void testSize() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        map.put("Key3", 3);
+        assertEquals(3, map.size());
+        System.out.println(map);
+    }
+
+    @Test
+    void testContainsKey() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertTrue(map.containsKey("Key1"));
+        assertFalse(map.containsKey("Key3"));
+        System.out.println(map);
+    }
+
+    @Test
+    void testContainsValue() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        assertTrue(map.containsValue(1));
+        assertFalse(map.containsValue(3));
+        System.out.println(map);
+    }
+
+    @Test
+    void testClear() {
+        map.put("Key1", 1);
+        map.put("Key2", 2);
+        map.put("Key3", 3);
+        map.clear();
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+        System.out.println(map);
+    }
 }
