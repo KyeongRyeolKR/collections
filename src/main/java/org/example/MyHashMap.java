698084457d60ecdb18972f86cb5e14f57aa2445b
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-20 09:08:56 +0900

    HashMap 직접 구현하기 시작

---
+++ src/main/java/org/example/MyHashMap.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MyHashMap<K, V> {
+
+    private Object[] array = new Object[10];
+    private int size;
+
+    public V put(K key1, V i) {
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+}
